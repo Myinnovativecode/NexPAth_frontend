7379443350df2b18efdd535440adc5e8a7265fd4
@Janvi <janviprajapat18022004@gmail.com>
2025-05-28 07:01:36 +0530

    Updated AuthModal UI and logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,16 @@
 // Sidebar.tsx
+import {
+  FaUser,
+  FaBriefcase,
+  FaChalkboardTeacher,
+  FaSignInAlt,
+  FaUserPlus,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
 import ashaLogo from "../assets/asha-logo.png";
 import React from "react";
 import "./Sidebar.css";
-
 
 interface SidebarProps {
   isOpen: boolean;
@@ -40,15 +48,27 @@ const Sidebar: React.FC<SidebarProps> = ({
         {userName ? (
           <>
             <li className="welcome-msg">👋 Welcome, {userName}</li>
-            <li onClick={onProfileClick}>My Profile</li>
-            <li onClick={handleFindJobs}>Find Jobs</li>
-            <li onClick={handleMentorship}>Get Mentorship</li>
-            <li onClick={onLogout} className="logout-btn">Logout</li>
+            <li onClick={onProfileClick}>
+              <FaUser style={{ marginRight: "8px" }} /> My Profile
+            </li>
+            <li onClick={handleFindJobs}>
+              <FaBriefcase style={{ marginRight: "8px" }} /> Find Jobs
+            </li>
+            <li onClick={handleMentorship}>
+              <FaChalkboardTeacher style={{ marginRight: "8px" }} /> Get Mentorship
+            </li>
+            <li onClick={onLogout} className="logout-btn">
+              <FaSignOutAlt style={{ marginRight: "8px" }} /> Logout
+            </li>
           </>
         ) : (
           <>
-            <li onClick={() => onAuthClick("login")}>Login</li>
-            <li onClick={() => onAuthClick("signup")}>Signup</li>
+            <li onClick={() => onAuthClick("login")}>
+              <FaSignInAlt style={{ marginRight: "8px" }} /> Login
+            </li>
+            <li onClick={() => onAuthClick("signup")}>
+              <FaUserPlus style={{ marginRight: "8px" }} /> Signup
+            </li>
           </>
         )}
       </ul>
@@ -57,5 +77,6 @@ const Sidebar: React.FC<SidebarProps> = ({
 };
 
 export default Sidebar;
+
 
 
